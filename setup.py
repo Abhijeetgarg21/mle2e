f7e8b5fdb83f4592ea0f2a9e30964bca88b7ecef
abhijeet0621 <21f1003267@ds.study.iitm.ac.in>
2025-12-28 00:22:20 +0530

    logger and exception

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requirement()->list[str]:
-    requirement_list = list[str] = []
+def get_requirement()->List[str]:
+    requirement_list : List[str] = []
     return requirement_list
 
 
